e1c395f788f152cc66748c4f2b16b72ecad88f7d
vietluongptit <148364913+vietluongptit@users.noreply.github.com>
2023-11-03 23:24:14 +0700

    Thêm ngày tháng năm vào list order.

--- web_fe/src/components/ListOrder.jsx
+++ web_fe/src/components/ListOrder.jsx
@@ -3,7 +3,51 @@ function ListOrder() {
 
     return (
         <>
-       <div class="product-cart-left">
+       <div class="product-cart-left" style={{border: "2px solid #000000", width: "70%", margin: "20px 200px"}}>
+       <div style={{ display: 'flex', justifyContent: 'center', marginTop:"20px", fontSize:20, color:"#da742a", fontWeight:1000}}>
+            <p>03/11/2023</p>
+        </div>
+        <hr/> 
+    <div class="item-product-cart">
+        <div class="checked-product-cart">
+           
+        </div>
+        <div class="img-product-cart">
+            <a class="product-image" href="https://www.fahasa.com/lich-am-duong-cat-tuong-2024.html">
+                <img src="https://cdn0.fahasa.com/media/catalog/product//9/7/9786044762135.jpg" width="120" height="120" alt="Lịch Âm Dương Cát Tường 2024"/>
+            </a>
+        </div>
+        <div class="group-product-info">
+            <div class="info-product-cart">
+                <div>
+                    <h2 class="product-name-full-text">
+                        <a href="https://www.fahasa.com/lich-am-duong-cat-tuong-2024.html">USER: fjhsdkjf</a>
+                    </h2>
+                </div>
+                <div class="price-original">
+                    <div class="cart-price">
+                        <div class="cart-fhsItem-price">
+                            <div>
+                                <span class="price">31.500 đ</span>
+                            </div>
+                            <div class="fhsItem-price-old">
+                                <span class="price">35.000 đ</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="number-product-cart">
+               
+                <div class="cart-price-total">
+                    <span class="cart-price">
+                        <a href='/bill' class="price">Chi tiết</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr/> 
     <div class="item-product-cart">
         <div class="checked-product-cart">
            
